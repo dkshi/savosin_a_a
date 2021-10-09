032fabc57713faeec8df442b922112544a5e836d
dkshi <gusgusevich@bk.ru>
2021-10-10 00:42:16 +0300

    hw

--- HW_2/level2_task3.dart
+++ HW_2/level2_task3.dart
@@ -2,18 +2,18 @@ import 'dart:io';
 
 void main() {
   String s = stdin.readLineSync()!;
-  String s1 = "";
+  print(func(s));
+}
+
+String func(s){
+  String s1 = s[0];
   for (int i = 0; i < s.length-1; i++){
     if (s[i] == ' ' && s[i + 1] == " "){
-      s[i + 1] == '';
+      continue;
     }
     else{
       s1 += s[i+1];
     }
   }
-  if (s[0] == " ")
-    s1 = " $s1";
-  else
-    s1 = "${s[0]}$s1";
-  print(s1);
+  return s1;
 }
