5f5cf92acf6c7c6647b47b55ca00611d4a9ee372
dkshi <90840579+dkshi@users.noreply.github.com>
2021-10-13 13:38:20 +0300

    Update level3_task1.dart

--- HW_2/level3_task1.dart
+++ HW_2/level3_task1.dart
@@ -1,5 +1,10 @@
 import 'dart:io';
 
 void main(){
-  
+  int n = int.parse(stdin.readLineSync()!);
+  List<int> fib = [0, 1];
+  for (int i = 2; i <= n; i++){
+    fib.add(fib[i-1]+fib[i-2]);
+  }
+  print(fib[n-1]);
 }
