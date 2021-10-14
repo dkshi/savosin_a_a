d7969ebcd8c9ecb07ef236a1f183209b733800af
dkshi <90840579+dkshi@users.noreply.github.com>
2021-10-14 18:04:06 +0300

    hw

---
+++ HW_3/test.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+String url(String server, String path, {String protocol = "http://", String port = "", String file = ""}){
+  return protocol + server + port + path + file;
+}
+
+
+
+void main(){
+  amogus(12);
+  12;
+}
+
+void amogus(int x){
+  print(x);
+}
