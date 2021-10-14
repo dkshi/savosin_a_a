3890b60385b6d5f3d0870c3c9b2d86cb5d8dd016
dkshi <90840579+dkshi@users.noreply.github.com>
2021-10-14 18:04:26 +0300

    hw

--- HW_2/level3_task1.dart
+++ HW_2/level3_task1.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
 
-void main(){
+void main() {
   int n = int.parse(stdin.readLineSync()!);
   List<int> fib = [0, 1];
-  for (int i = 2; i <= n; i++){
-    fib.add(fib[i-1]+fib[i-2]);
+  for (int i = 2; i <= n; i++) {
+    fib.add(fib[i - 1] + fib[i - 2]);
   }
-  print(fib[n-1]);
+  print(fib[n - 1]);
 }
